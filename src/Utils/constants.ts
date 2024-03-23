c27e557f71f6a4f42e30dc9aa53584d3971dd438
Pillippa <ignacia.pp+github@gmail.com>
2024-03-23 10:04:29 -0700

    changed backend url

--- src/Utils/constants.ts
+++ src/Utils/constants.ts
@@ -1,3 +1,3 @@
-export const PARTYKIT_HOST = "https://test-partykit.pillin.partykit.dev/";
+export const PARTYKIT_HOST = "https://survey-sync-backend.pillin.partykit.dev/";
 export const PARTYKIT_FRONTEND_HOST = "https://test-solid-partykit-gnrz.vercel.app/";
 export const ROOM = "my-new-room";
